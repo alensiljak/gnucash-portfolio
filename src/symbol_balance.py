c0667d93e50b8d58dd5ee6d8d1e526dedbb86332
Alen Siljak <alen.siljak@gmx.com>
2017-11-16 16:32:29 +0100

    trying to get the balance per security

---
+++ src/symbol_balance.py
@@ -0,0 +1,39 @@
+"""
+Displays the balance of a security across all accounts.
+"""
+import sys
+from piecash import Commodity
+from lib import database
+
+def main(symbol):
+	"""
+	Displays the balance for the security symbol.
+	"""
+	#print("ok, you thought this would be easy?")
+	print("Displaying the balance for", symbol)
+	db = database.Database()
+	with db.open_book() as book:
+		security = book.get(Commodity, mnemonic=symbol)
+		#print(security.transactions)
+		# security.prices
+		total = 0
+		for account in security.accounts:
+			balance = account.get_balance()
+			print(account.name, balance)
+			total += balance
+		
+		print(total)
+
+#############################################################
+# get the name of the security
+symbol = None
+
+if symbol:
+	# When debugging, adjust the symbol manually.
+	main(symbol)
+else:
+	if len(sys.argv) == 1:
+		print("You need to provide the symbol for the security to be displayed.")
+	else:
+		symbol = sys.argv[1]
+		main(symbol)
