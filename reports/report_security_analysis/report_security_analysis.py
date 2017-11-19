6bae6ccd32db79957a9428d1fc9a9b359bcfbcc8
Alen Siljak <alen.siljak@gmx.com>
2017-11-19 15:45:18 +0100

    adding the selector for commodities

--- reports/report_security_analysis/report_security_analysis.py
+++ reports/report_security_analysis/report_security_analysis.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Security Analysis
 Displays the quantity of the selected commodity and the average price paid.
@@ -7,8 +7,9 @@ import sys
 import os
 import piecash
 from piecash import Commodity
-from piecash_utilities.report import report, execute_report
-import symbol_balance
+from piecash_utilities.report import report, execute_report, CommodityOption, CommodityListOption
+#import symbol_balance
+from decimal import Decimal
 
 ####################################################################
 @report(
@@ -17,18 +18,34 @@ import symbol_balance
     menu_tip="Security details",
     options_default_section="general",
 )
-def generate_report(book_url):
+def generate_report(book_url,
+                    commodity: CommodityOption(
+                        section="Commodity",
+                        sort_tag="a",
+                        documentation_string="This is a stock",
+                        default_value="VTIP"),
+                    commodity_list: CommodityListOption(
+                        section="Commodity",
+                        sort_tag="a",
+                        documentation_string="This is a stock",
+                        default_value="VTIP")
+                    ):
     """
     Generates an HTML report content.
     """
+    print("Symbol requested:", commodity)
+    #print(commodity.name)
+    print(commodity_list)
+
+    # TODO replace this with the received parameter / list.
     symbol = "VTIP"
     shares_no = None
     avg_price = None
 
     with piecash.open_book(book_url, readonly=True, open_if_lock=True) as book:
         security = book.get(Commodity, mnemonic=symbol)
-        shares_no = symbol_balance.get_number_of_shares(security)
-        avg_price = symbol_balance.get_avg_price(security)
+        shares_no = get_number_of_shares(security)
+        avg_price = get_avg_price(security)
 
     # Load HTML template file.
     template = load_html_template()
@@ -44,6 +61,55 @@ def load_html_template():
 
     with open(file_path, 'r') as template_file:
         return template_file.read()
+
+def get_avg_price(security):
+    """
+    Calculates the average price paid for the security.
+    security = Commodity
+    """
+    avg_price = Decimal(0)
+
+    #return sum([sp.quantity for sp in self.splits]) * self.sign
+
+    for account in security.accounts:
+        # Ignore trading accounts.
+        if account.type == "TRADING":
+            continue
+
+        price_total = Decimal(0)
+        price_count = 0
+
+        for split in account.splits:
+            price = split.value / split.quantity
+            #print(price)
+            price_count += 1
+            price_total += price
+
+    avg_price = price_total / price_count
+    return avg_price
+
+def get_number_of_shares(security):
+    """
+    Returns the number of shares for the given security.
+    It gets the number from all the accounts in the book.
+    """
+    total_quantity = Decimal(0)
+    #total_balance = Decimal(0)
+
+    for account in security.accounts:
+        # exclude Trading accouns explicitly.
+        if account.type == "TRADING":
+            continue
+
+        balance = account.get_balance()
+        quantity = account.get_quantity()
+
+        #print(account.fullname, quantity, balance)
+        #total_balance += balance
+        total_quantity += quantity
+
+    #print("Balance:", total_balance)
+    return total_quantity
 
 ####################################################################
 if __name__ == '__main__':
