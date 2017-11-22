a919968813ff93e97938e728d797fe7bf7ff721a
Alen Siljak <alen.siljak@gmx.com>
2017-11-22 10:58:32 +0100

    moving examples to examples folder; starting asset allocation report; adding the script to deploy the reports

---
+++ reports/deploy.py
@@ -0,0 +1,32 @@
+"""
+Copies the reports from this directory into .gnucash directory in the profile directory.
+After this is performed, run `gc_report` the first time or if the parameters have changed.
+"""
+import os
+#import shutil
+import distutils
+from distutils import dir_util
+from os.path import expanduser
+from os import path
+
+# get all the report folders
+report_folders = []
+#subdirs = os.listdir(".")
+#subdirs = os.walk(".")
+subdirs = os.scandir(".")
+for subdir in subdirs:
+    if not subdir.is_dir():
+        continue
+    #print(subdir)
+    report_folders.append(subdir)
+
+# TODO copy to the destination
+home = expanduser("~")
+gnucash_dir = path.abspath(home + "/.gnucash/")
+
+for report_dir in report_folders:
+    report_name = report_dir.name
+    dst = path.join(gnucash_dir, report_name)
+    #shutil.copytree(report_name, dst)
+    distutils.dir_util.copy_tree(report_name, dst)
+    print("Copied", report_name)
---
+++ reports/report_asset_allocation/report_asset_allocation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Asset Allocation report.
+Asset Allocation is stored in the accompanying .json file and needs to be updated manually.
+"""
+import os
+import sys
+import piecash
+from piecash_utilities.report import report, execute_report
+
+@report(
+    title="Asset Allocation",
+    name="asset-allocation",
+    menu_tip="Asset Allocation report",
+    options_default_section="general",
+)
+def generate_report(book_url):
+    """
+    Generates the HTML report output.
+    """
+    return generate_asset_allocation_report(book_url)
+
+def generate_asset_allocation_report(book_url):
+    """
+    The otput is generated here. Separeted from the generate_report function to allow executing
+    from the command line.
+    """
+    # TODO read asset allocation file
+    # TODO read security information from the book.
+    # TODO calculate allocation in the book.
+
+    return "YO!"
+
+###################################################
+if __name__ == "__main__":
+    """
+    Invoked from the command line.
+    """
+    # TODO Get the test book url.
+    book_url = None
+    result = generate_asset_allocation_report(book_url)
+    print(result)
+    #execute_report(generate_report, sys.argv[1])
