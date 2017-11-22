8b050614a41724ad93f76d801cac28085ab5e2ff
Alen Siljak <alen.siljak@gmx.com>
2017-11-22 11:23:15 +0100

    scripts to make gnucash-portfolio installable via pip -e install <path>

---
+++ src/metadata.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'gnucash-portfolio'
+project = "Portfolio tools for GnuCash"
+project_no_spaces = project.replace(' ', '')
+version = '0.0.1'
+description = 'Portfolio tools for GnuCash'
+authors = ['alen.siljak']
+authors_string = ', '.join(authors)
+emails = ['alen.siljak@gmx.com']
+license = 'GPL3'
+copyright = '2017 ' + authors_string
+url = 'https://github.com/MisterY/gnucash-portfolio'
