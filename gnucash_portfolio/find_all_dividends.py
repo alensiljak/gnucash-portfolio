c90b5a1ed752a6e43c7c8423a271e3fa08e158fa
Alen Siljak <alen.siljak@gmx.com>
2017-12-07 20:57:01 +0100

    calculate all income per symbol

--- gnucash_portfolio/find_all_dividends.py
+++ gnucash_portfolio/find_all_dividends.py
@@ -4,10 +4,15 @@ as the symbol.
 This method does not require explicit linking of distribution payments to the
 stock (commodity).
 """
+from decimal import Decimal
 from gnucash_portfolio.lib import database
-from piecash import Account, Commodity, Book
+from piecash import Account, Commodity, Book, Split
 
-def get_dividend_accounts(book:Book, symbol:str):
+def get_dividend_accounts(book: Book, symbol: str):
+    """
+    Finds all the distribution accounts (they are in Income group and have the same name
+    as the stock symbol).
+    """
     # get the stock
     stock = book.session.query(Commodity).filter(Commodity.mnemonic == symbol)
 
@@ -21,6 +26,32 @@ def get_dividend_accounts(book:Book, symbol:str):
     return income_accounts
 
 
+def get_dividend_sum(book: Book, account: Account):
+    """    Adds all distributions (income)    """
+    splits = book.session.query(Split).filter(Split.account == account).all()
+    dividend_sum = Decimal(0)
+
+    for split in splits:
+        dividend_sum += split.value
+        # debug print split...
+
+    # Since the income comes from the distribution account, it is marked as debit.
+    return dividend_sum * (-1)
+
+
+def get_dividend_sum_for_symbol(book: Book, symbol: str):
+    """    Calculates all income for a symbol    """
+    accounts = get_dividend_accounts(book, symbol)
+    total = Decimal(0)
+
+    for account in accounts:
+        # get all dividends.
+        income = get_dividend_sum(book, account)
+        total += income
+
+    return total
+
+
 if __name__ == "__main__":
     book_path: str = input("Enter book path (or leave empty):")
     symbol: str = input("Enter symbol:")
@@ -32,5 +63,6 @@ if __name__ == "__main__":
         db = database.Database()
 
     with db.open_book() as book:
-        accounts = get_dividend_accounts(book, symbol)
-        print(accounts)
+        amount = get_dividend_sum_for_symbol(book, symbol)
+        
+        print("Income for", symbol, "=", amount)
