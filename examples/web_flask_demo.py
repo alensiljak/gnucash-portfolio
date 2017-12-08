da66df276108cbd00b2e033a6ef2d6d11e69c286
Alen Siljak <alen.siljak@gmx.com>
2017-12-08 11:17:35 +0100

    rendering a template with flask

--- examples/web_flask_demo.py
+++ examples/web_flask_demo.py
@@ -2,8 +2,8 @@
 Demonstrate web page with Flask
 """
 from flask import Flask
-
 app = Flask(__name__)
+from flask import render_template
 
 #from app import routes
 #from app import app
@@ -12,7 +12,11 @@ app = Flask(__name__)
 #@app.route('/index')
 def index():
     """Default output"""
-    return "Hello, World!"
+    user = { 'nickname': 'Miguel' }
+    #return "Hello, World!"
+    return render_template('web_flask_demo.html',
+                            title='Home',
+                            user=user)
 
 if __name__ == "__main__":
     app.run()
