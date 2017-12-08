95bda1d06c3ec6f6c76c7dc3abc424c6b1595df0
Alen Siljak <alen.siljak@gmx.com>
2017-12-09 00:02:09 +0100

    templated view decorator

--- app/app.py
+++ app/app.py
@@ -2,10 +2,11 @@
 This is the entry point to the application
 """
 import sys
+from decorators import templated
 #import logging
 from flask import Flask, render_template, request
 from gnucash_portfolio import get_vanguard_au_prices
-from gnucash_portfolio.lib import assetallocation, settings, portfoliovalue, database
+from gnucash_portfolio.lib import assetallocation as aalloc, settings, portfoliovalue as pvalue, database
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -19,7 +20,8 @@ def index():
 
 
 @app.route('/vanguardprices')
-def vanguard():
+@templated()
+def vanguardprices():
     """ Prices for Vanguard funds """
     #funds = "8123,8146,8148,8147"
     #print(request.form.get("funds"))
@@ -31,30 +33,36 @@ def vanguard():
         user_funds = funds.strip().split(",")
         prices = get_vanguard_au_prices.download_fund_prices(user_funds)
 
-    return render_template('vanguard_prices.html', prices=prices, funds=funds)
+    #return render_template('vanguard_prices.html', prices=prices, funds=funds)
+    return dict(prices=prices, funds=funds)
 
 
 @app.route('/assetallocation')
-def asset_allocation():
+@templated()
+def assetallocation():
     """ Asset Allocation """
     book_url = settings.Settings().database_uri
-    model = assetallocation.load_asset_allocation_model(book_url)
-    return render_template('assetallocation.html', model=model)
+    model = aalloc.load_asset_allocation_model(book_url)
+    #return render_template('assetallocation.html', model=model)
+    return dict(model=model)
 
 
 @app.route('/portfoliovalue')
-def portfolio_value():
+@templated()
+def portfoliovalue():
     """ Portfolio Value report """
     stock_rows = []
     with database.Database().open_book() as book:
-        all_stocks = portfoliovalue.get_all_stocks(book)
+        all_stocks = pvalue.get_all_stocks(book)
         #print("found ", len(all_stocks), "records")
         for stock in all_stocks:
-            model = portfoliovalue.get_stock_model_from(book, stock)
+            model = pvalue.get_stock_model_from(book, stock)
             stock_rows.append(model)
 
     #print(stock_rows)
-    return render_template('portfolio_value.html', stock_rows=stock_rows)
+    #return render_template('portfolio_value.html', stock_rows=stock_rows)
+    return dict(stock_rows=stock_rows)
+
 
 ##################################################################################
 if __name__ == '__main__':
---
+++ app/decorators.py
@@ -0,0 +1,25 @@
+"""
+Decorators for Flask
+"""
+from functools import wraps
+from flask import request, render_template
+
+def templated(template=None):
+    """
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/#templating-decorator
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            template_name = template
+            if template_name is None:
+                template_name = request.endpoint \
+                    .replace('.', '/') + '.html'
+            ctx = f(*args, **kwargs)
+            if ctx is None:
+                ctx = {}
+            elif not isinstance(ctx, dict):
+                return ctx
+            return render_template(template_name, **ctx)
+        return decorated_function
+    return decorator
