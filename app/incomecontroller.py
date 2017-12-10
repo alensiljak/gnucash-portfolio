7303331f58b2f78bf4dcfd36238ea49dff503dd1
Alen Siljak <alen.siljak@gmx.com>
2017-12-10 22:42:38 +0100

    using direct query to easier filter data

--- app/incomecontroller.py
+++ app/incomecontroller.py
@@ -1,11 +1,13 @@
 """
 Income reports
 """
+from typing import List
 from datetime import date
 import dateutil
-from decimal import Decimal
+#from decimal import Decimal
 from flask import Blueprint, request, render_template
-from piecash import Account, Split, Book
+#from sqlalchemy.dialects import sqlite
+from piecash import Account, Split, Book, Transaction
 from gnucash_portfolio.lib import database
 
 income_controller = Blueprint('income_controller', __name__,
@@ -15,31 +17,32 @@ income_controller = Blueprint('income_controller', __name__,
 @income_controller.route('/inperiod')
 def income_in_period():
     """ Investment income in time period """
-    # Check if there are parameters.
-    date_from = None
-    from_str = request.args.get("datefrom")
-    if from_str:
-        date_from = dateutil.parser.parse(from_str)
-    to_str = request.args.get("dateto")
-    if to_str:
-        date_to = dateutil.parser.parse(to_str)
+    input_model = __get_input_model()
 
-    # TODO: Allow selecting the income accounts. Save to the settings.
+    # Allow selecting the income accounts.
+    # TODO Save to the settings?
     income_accounts = [
         "Income:Investment"
     ]
 
-    model = None
-    if date_from:
+    model = {}
+    if "date_from" in input_model:
+        model = {
+            "datefrom": input_model["date_from_str"],
+            "dateto": input_model["date_to_str"]
+        }
         # load data
         with database.Database().open_book() as book:
-            transactions = __load_income_in_period(
-                book, income_accounts, date_from, date_to)
-            model = {
-                "splits": transactions,
-                "datefrom": from_str,
-                "dateto": to_str
-            }
+            #            splits = __load_income_in_period(
+            #                book, income_accounts, date_from, date_to)
+            # Sort by date
+            #            splits.sort(key=lambda split: split.transaction.post_date)
+
+            account_ids = __get_income_account_ids(book, income_accounts)
+            splits = __load_income_in_period_query(book, account_ids,
+                                                   input_model["date_from"], input_model["date_to"])
+
+            model["splits"] = splits
             output = render_template('income_in_period.html', model=model)
     else:
         output = render_template('income_in_period.html', model=model)
@@ -47,12 +50,30 @@ def income_in_period():
     return output
 
 
-def __load_income_in_period(book: Book, account_fullnames: list, date_from: date, date_to: date):
+def __get_input_model():
+    """ Parses user input """
+    model = {}
+
+    from_str = request.args.get("datefrom")
+    if from_str:
+        model["date_from"] = dateutil.parser.parse(from_str)
+        model["date_from_str"] = from_str
+
+    to_str = request.args.get("dateto")
+    if to_str:
+        model["date_to"] = dateutil.parser.parse(to_str)
+        model["date_to_str"] = to_str
+
+    return model
+
+
+def __load_income_in_period(
+        book: Book, account_fullnames: List[str], date_from: date, date_to: date):
     """ load income transactions in the given period """
     income_transactions = []
 
     # locate income account(s)
-    income_accounts = __get_income_accounts(book, account_fullnames)
+    income_accounts = __get_accounts_by_name(book, account_fullnames)
 
     # load data
     for acc in income_accounts:
@@ -63,7 +84,7 @@ def __load_income_in_period(book: Book, account_fullnames: list, date_from: date
     return income_transactions
 
 
-def __get_income_accounts(book: Book, account_fullnames: list):
+def __get_accounts_by_name(book: Book, account_fullnames: List[str]) -> List[Account]:
     """ retrieves the account objects for the given full names """
     result = []
 
@@ -74,10 +95,41 @@ def __get_income_accounts(book: Book, account_fullnames: list):
     return result
 
 
+def __get_ids(accounts: List[Account]):
+    """ extracts only the ids from the account list """
+    return [o.guid for o in accounts]
+
+
+def __get_income_account_ids(book: Book, account_names: List[str]) -> List[str]:
+    """ Loads all income account ids """
+    accounts = __get_accounts_by_name(book, account_names)
+    ids = []
+
+    for account in accounts:
+        child_ids = __get_children_ids(account)
+        ids += child_ids
+
+    return ids
+
+
+def __get_children_ids(account: Account) -> List[str]:
+    """ recursive function that loads ids from all child accounts in the tree """
+    ids = []
+
+    if not account.placeholder:
+        ids.append(account.guid)
+
+    for child in account.children:
+        child_ids = __get_children_ids(child)
+        ids += child_ids
+    return ids
+
+
 def __load_all_income_for_account(account: Account, date_from: date, date_to: date):
     """ Loads all income in the account """
     result = []
-    for split in account.splits:
+    splits_in_period = account.splits()
+    for split in splits_in_period:
         result.append(split)
 
     # load transactions for all child accounts
@@ -87,3 +139,20 @@ def __load_all_income_for_account(account: Account, date_from: date, date_to: da
         result += child_splits
 
     return result
+
+
+def __load_income_in_period_query(
+        book: Book, account_ids: List[hex],
+        date_from: date, date_to: date) -> List[Split]:
+    """ Load all data by using the query directly """
+    query = (book.query(Split)
+             .join(Transaction)
+             .join(Account)
+             .filter(Transaction.post_date >= date_from, Transaction.post_date <= date_to,
+                     Account.guid.in_(account_ids))
+             .order_by(Transaction.post_date)
+            )
+    # Check the generated SQL
+#    sql = str(query.statement.compile(dialect=sqlite.dialect(),
+#       compile_kwargs={"literal_binds": True}))
+    return query.all()
