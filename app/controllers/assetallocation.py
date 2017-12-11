d9ca4d75210474418f0f86d5ad89fadef62b5819
Alen Siljak <alen.siljak@gmx.com>
2017-12-11 17:08:24 +0100

    starting new method for asset classes only

--- app/controllers/assetallocation.py
+++ app/controllers/assetallocation.py
@@ -6,11 +6,14 @@ from gnucash_portfolio.lib import settings, assetallocation as aalloc
 
 assetallocation_controller = Blueprint('assetallocation_controller', __name__, url_prefix='/assetallocation')
 
-@assetallocation_controller.route('/')
-#@templated()
+@assetallocation_controller.route('/full')
 def assetallocation():
-    """ Asset Allocation """
+    """ Asset Allocation with stocks """
     book_url = settings.Settings().database_uri
     model = aalloc.load_asset_allocation_model(book_url)
     return render_template('assetallocation.html', model=model)
-    #return dict(model=model)
+
+@assetallocation_controller.route('/')
+def asset_allocation():
+    """ Asset Allocation without the securities """
+    return render_template('asset_allocation.html', model=None)
