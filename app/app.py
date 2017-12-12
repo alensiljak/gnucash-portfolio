4190a962cc7bd7425798d15fc4e146f59bfb765e
Alen Siljak <alen.siljak@gmx.com>
2017-12-12 16:57:18 +0100

    adjusting path for popper.js, all paths now correct

--- app/app.py
+++ app/app.py
@@ -33,7 +33,7 @@ bundles = {
         '../node_modules/daterangepicker/daterangepicker.css',
         output='vendor.css'),
     'vendor_js': Bundle(
-        '../node_modules/popper.js/dist/popper.min.js',
+        '../node_modules/popper.js/dist/umd/popper.min.js',
         '../node_modules/jquery/dist/jquery.min.js',
         '../node_modules/moment/min/moment.min.js',
         '../node_modules/daterangepicker/daterangepicker.js',
