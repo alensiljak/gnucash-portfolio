206dcae28fae0e753909c512118dbadf12740056
Alen Siljak <alen.siljak@gmx.com>
2017-12-23 00:28:38 +0100

    setting defaults

--- app/models/transaction_models.py
+++ app/models/transaction_models.py
@@ -6,14 +6,14 @@ from types import SimpleNamespace
 
 # Input model for scheduled transactions list.
 ScheduledTxSearchModel = SimpleNamespace( # pylint: disable=invalid-name
-    'date_from',
-    'date_to'
+    date_from=None,
+    date_to=None
 )
 
 
 # result row for scheduled transactions.
 SchedTxRowViewModel = SimpleNamespace( # pylint: disable=invalid-name
-    'name',
-    'start_date',
-    'last_occurred'
+    name=None,
+    start_date=None,
+    last_occurred=None
 )
