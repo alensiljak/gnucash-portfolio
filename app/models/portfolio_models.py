464779b00bc3fdaacd64891bd84b6502a272a7bb
Alen Siljak <alen.siljak@gmx.com>
2017-12-23 04:00:49 +0100

    lint adjustments; removing empty links

--- app/models/portfolio_models.py
+++ app/models/portfolio_models.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class PortfolioValueInputModel(): # pylint: disable=invalid-name
+class PortfolioValueInputModel(): # pylint: disable=invalid-name,too-few-public-methods
     """ Search model for Portfolio Value """
     as_of_date: datetime = datetime(datetime.today().year,
                                     datetime.today().month,
@@ -14,7 +14,7 @@ class PortfolioValueInputModel(): # pylint: disable=invalid-name
 
 
 @dataclass
-class PortfolioValueViewModel(): # pylint: disable=invalid-name
+class PortfolioValueViewModel(): # pylint: disable=invalid-name,too-few-public-methods
     """ View Model for portfolio value report """
     filter = None
     stock_rows = []
--- app/models/transaction_models.py
+++ app/models/transaction_models.py
@@ -4,14 +4,14 @@ from dataclasses import dataclass
 
 
 @dataclass
-class ScheduledTxSearchModel: # pylint: disable=invalid-name
+class ScheduledTxSearchModel: # pylint: disable=invalid-name,too-few-public-methods
     """ Input model for scheduled transactions list """
     date_from = None
     date_to = None
 
 
 @dataclass
-class SchedTxRowViewModel: # pylint: disable=invalid-name
+class SchedTxRowViewModel: # pylint: disable=invalid-name,too-few-public-methods
     """ result row for scheduled transactions """
     name = None
     start_date = None
