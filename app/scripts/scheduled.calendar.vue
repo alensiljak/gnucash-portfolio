542a7d20aa38ec983798086107e2b35e83d570b8
Alen Siljak <alen.siljak@gmx.com>
2018-01-14 15:52:04 +0100

    calendar demo!

--- app/scripts/scheduled.calendar.vue
+++ app/scripts/scheduled.calendar.vue
@@ -2,27 +2,45 @@
   Scheduled Transactions Calendar
 -->
 <template>
+<div>
   <p>Calendar</p>
+  <full-calendar :events="events"></full-calendar>
+</div>
 </template>
 
 <script>
-import Vue from 'vue';
+import Vue from "vue";
 import { FullCalendar } from "vue-full-calendar";
 Vue.use(FullCalendar);
 
 export default {
   data() {
     return {
-
-    }
+      events: [
+        {
+          title: "event1",
+          start: "2010-01-01"
+        },
+        {
+          title: "event2",
+          start: "2010-01-05",
+          end: "2010-01-07"
+        },
+        {
+          title: "event3",
+          start: "2010-01-09T12:30:00",
+          allDay: false
+        }
+      ]
+    };
   },
   components: {
-    FullCalendar,
+    FullCalendar
   }
 };
 </script>
 
 <style>
-
+@import "~fullcalendar/dist/fullcalendar.css";
 </style>
  
