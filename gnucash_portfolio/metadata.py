a4489c974863873a939c8e1fa7840444ebed7462
Alen Siljak <alen.siljak@gmx.com>
2018-01-14 16:34:29 +0100

    calendar deserves a minor version

--- gnucash_portfolio/metadata.py
+++ gnucash_portfolio/metadata.py
@@ -9,7 +9,7 @@ Information describing the project.
 package = 'gnucash-portfolio'
 project = "Portfolio tools for GnuCash"
 project_no_spaces = project.replace(' ', '')
-version = '0.2.0'
+version = '0.3.0'
 description = 'Portfolio tools for GnuCash'
 authors = ['alen.siljak']
 authors_string = ', '.join(authors)
