7f2c98bb5eb824394a53cd95eff042916b95d020
Alen Siljak <alen.siljak@gmx.com>
2018-01-14 14:53:20 +0100

    the initial implementation of scheduled transactions complete. raising the version number

--- gnucash_portfolio/metadata.py
+++ gnucash_portfolio/metadata.py
@@ -9,7 +9,7 @@ Information describing the project.
 package = 'gnucash-portfolio'
 project = "Portfolio tools for GnuCash"
 project_no_spaces = project.replace(' ', '')
-version = '0.1.0'
+version = '0.2.0'
 description = 'Portfolio tools for GnuCash'
 authors = ['alen.siljak']
 authors_string = ', '.join(authors)
