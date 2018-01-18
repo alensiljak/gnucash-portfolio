9b72c2a6f90e7a932b0b3341ff25682431d5e824
Alen Siljak <alen.siljak@gmx.com>
2018-01-18 10:06:47 +0100

    #28 functional calculator

--- app/scripts/currency.calculator.vue
+++ app/scripts/currency.calculator.vue
@@ -3,109 +3,115 @@ Currency calculator
 </docs>
 
 <template>
-
 <div>
-  <div class="row w-100">
-      <div class="form-group mx-auto">
-          <label>Amount</label>
-          <input id="amount" class="form-control text-right"
-            v-model.number="amount" @change="onSelect" @focus="$event.target.select()">
-      </div>
-  </div>
-  <div class="row mt-2">
-      <div class="col">
-          <div class="card">
-            <div class="card-header">
-              Source Currency
+    <div class="card">
+        <div class="card-header">
+            FX Calculator
+        </div>
+        <div class="card-body">
+            <div class="row">
+
+                <div class="col">
+                    <div class="form-group">
+                        <label for="srcCurrency" class="mr-2">Source Currency</label>
+                        <model-select :options="listModel" v-model="srcCurrency" placeholder="source currency" @input="onSelect">
+                        </model-select>
+                        
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="srcCurrency" class="mr-2">Destination Currency</label>
+                        <model-select :options="listModel" v-model="dstCurrency" placeholder="destination currency" @select="onSelect">
+                        </model-select>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-              <div class="form-group">
-                <label for="srcCurrency" class="mr-2">Currency symbol</label>
-                <model-select :options="listModel"
-                                v-model="srcCurrency"
-                                placeholder="select item"
-                                @select="onSelect">
-                </model-select>
-              </div>
+                <div class="row">
+                  <div class="col">
+                    <div class="form-group">
+                        <label>Amount</label>
+                        <input id="amount" class="form-control text-right" v-model.number="amount" @change="onSelect" @focus="$event.target.select()">
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <!-- Result -->
+                    <div class="form-group">
+                        <label>Result</label>
+                        <input id="result" readonly="readonly" class="form-control text-right" v-model="result">
+                    </div>
+                </div>
+                </div>
+
             </div>
-          </div>
-      </div>
-      <div class="col">
-          <div class="card">
-            <div class="card-header">
-              Destination Currency
+        </div>
+        <div class="row w-100 mt-2">
+            <div class="mx-auto">
+                <button class="btn btn-primary" @click="recalculate">Calculate</button>
             </div>
-            <div class="card-body">
-              <div class="form-group">
-                <label for="srcCurrency" class="mr-2">Currency symbol</label>
-                <model-select :options="listModel"
-                                v-model="dstCurrency"
-                                placeholder="select item"
-                                @select="onSelect">
-                </model-select>
-              </div>
-            </div>
-          </div>
-      </div>
-  </div>
-  <div class="row w-100 mt-2">
-    <div class="mx-auto">
-      <button class="btn btn-primary" @click="recalculate">Calculate</button>
+        </div>
     </div>
-  </div>
-  <!-- Result -->
-  <div class="row w-100 mt-3">
-      <div class="form-group mx-auto">
-          <label>Result</label>
-          <input id="result" readonly="readonly" class="form-control text-right">
-      </div>
-  </div>
 </div>
 
 </template>
 <script>
-import axios from 'axios'
-import { ModelSelect } from 'vue-search-select'
+import axios from "axios";
+import { ModelSelect } from "vue-search-select";
 
 export default {
   data() {
     return {
-      amount: 0,
       currencies: {},
       listModel: [],
       srcCurrency: {
-        value: '',
-        text: ''
+        value: "",
+        text: ""
       },
       dstCurrency: {
-        value: '',
-        text: ''
-      }
+        value: "",
+        text: ""
+      },
+      amount: 0,
+      amountInBase: 0, // Amount in base currency
+      result: 0
     };
   },
-  props: {
-
-  },
+  props: {},
   methods: {
     onSelect: function(item) {
       // TODO: recalculate the amount
-      console.log("selected")
+      console.log("selected");
 
-      this.recalculate()
+      this.recalculate();
     },
     recalculate: function() {
-      if (!this.amount || !this.srcCurrency.value || !this.dstCurrency.value) return;
+      // console.debug("recalculating...", this.amount, this.srcCurrency.value, this.dstCurrency.value)
+
+      if (!this.amount || !this.srcCurrency.text || !this.dstCurrency.text) {
+        // console.log("input missing. Exiting.")
+        return;
+      }
 
       // get source
-      console.log(JSON.stringify(this.srcCurrency))
+      // console.log(JSON.stringify(this.srcCurrency));
+      if (this.srcCurrency.text === "EUR") {
+        // already base currency
+        this.amountInBase = this.amount;
+      } else {
+        // recalculate to base currency
+        this.amountInBase = this.amount * this.srcCurrency.value;
+      }
 
-      // get destination
-      console.log(JSON.stringify(this.dstCurrency))
+      // calculate destination
+      // console.log(JSON.stringify(this.dstCurrency));
+      this.result = this.amountInBase / this.dstCurrency.value;
+      // round to 2 decimals
+      this.result = Math.round(this.result * 10) / 10;
 
-      // recalculate?
-      // should this be a server-side operation?
-      console.log("recalculating here")
-    },
+      // recalculate
+      // console.log("recalculation complete");
+    }
   },
   mounted: function mounted() {
     // Initialization.
@@ -114,7 +120,8 @@ export default {
     $("#amount").focus();
 
     // load currencies
-    axios.get('/currency/api/rates')
+    axios
+      .get("/currency/api/rates")
       .then(function(response) {
         console.log("rates fetched");
         self.currencies = response.data;
@@ -125,7 +132,7 @@ export default {
         }
 
         // refresh the selectors
-        $('#srcCurrency').trigger("chosen:updated");
+        $("#srcCurrency").trigger("chosen:updated");
         // $('#srcCurrency').chosen();
       })
       .catch(function(error) {
@@ -138,4 +145,5 @@ export default {
 };
 </script>
 <style>
+
 </style>
