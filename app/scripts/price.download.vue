2624bb39fd2b3684940255a7faad7b1b955b3895
Alen Siljak <alen.siljak@gmx.com>
2018-01-19 12:10:56 +0100

    #24 progressive display steps

--- app/scripts/price.download.vue
+++ app/scripts/price.download.vue
@@ -8,6 +8,20 @@
           </div>
           <div class="card-body">
               <button class="btn" @click="fetchPrice">Fetch</button>
+
+            <div v-show="price">
+              <div class="form-group">
+                  <label>Current Price:</label>
+              <input id="currentPrice" class="form-control" v-model="price" />
+              </div>
+
+              <div>
+                  todo: select currency
+              </div>
+              <div>
+                  todo: import (send the values to an api endpoint)
+              </div>
+            </div>
           </div>
       </div>
   </div>
@@ -16,32 +30,37 @@
 {{ test }}
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
-    data() {
-        return {
-            symbol: ""
-        }
-    },
-    props: {
-    },
+  data() {
+    return {
+      symbol: "",
+      price: null
+    };
+  },
+  props: {},
 
-    // created: () => console.log("created"),
-    mounted: function() {
-        this.symbol = window.model.symbol
-    },
+  // created: () => console.log("created"),
+  mounted: function() {
+    this.symbol = window.model.symbol;
+  },
 
-    methods: {
-        fetchPrice: function() {
-            axios.get('/')
-                .then(function(response) {
-                    // test for 200?
-                    console.log(response.data)
-                })
-        }
+  methods: {
+    fetchPrice: function() {
+      // using Morningstar.
+
+      axios.get("/").then(response => {
+        // test for 200?
+        this.price = this.parseMsHtml(response.data);
+      });
+    },
+    parseMsHtml: function(html) {
+      // todo: get the price
+      return html;
     }
-}
+  }
+};
 </script>
 
 <style>
