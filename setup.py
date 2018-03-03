8f6737983d2c7a05cdd6daf897634b78a1399932
Alen Siljak <alen.siljak@gmx.com>
2018-03-03 23:07:55 +0100

    setting up a package. v0.5.0

--- setup.py
+++ setup.py
@@ -1,332 +1,177 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
+"""
+A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-import imp
-import os
-import subprocess
-import sys
-
-# # Python 2.6 subprocess.check_output compatibility. Thanks Greg Hewgill!
-from os.path import join as pjoin, splitext, split as psplit
-from distutils.command.install_scripts import install_scripts
-from distutils import log
-
-if 'check_output' not in dir(subprocess):
-    def check_output(cmd_args, *args, **kwargs):
-        proc = subprocess.Popen(
-            cmd_args, *args,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, **kwargs)
-        out, err = proc.communicate()
-        if proc.returncode != 0:
-            raise subprocess.CalledProcessError(args)
-        return out
-
-
-    subprocess.check_output = check_output
-
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-try:
-    import colorama
+here = path.abspath(path.dirname(__file__))
 
-    colorama.init()  # Initialize colorama on Windows
-except ImportError:
-    # Don't require colorama just for running paver tasks. This allows us to
-    # run `paver install' without requiring the user to first have colorama
-    # installed.
-    pass
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-# Add the current directory to the module search path.
-sys.path.append('.')
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
 
-# # Constants
-CODE_DIRECTORY = 'gnucash_portfolio'
-DOCS_DIRECTORY = 'doc'
-TESTS_DIRECTORY = 'tests'
-DATA_DIRECTORY = 'src/data'
-PYTEST_FLAGS = ['--doctest-modules']
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name='gnucash_portfolio',  # Required
 
-# Import metadata. Normally this would just be:
-#
-# from piecash import metadata
-#
-# However, when we do this, we also import `piecash/__init__.py'. If this
-# imports names from some other modules and these modules have third-party
-# dependencies that need installing (which happens after this file is run), the
-# script will crash. What we do instead is to load the metadata module by path
-# instead, effectively side-stepping the dependency problem. Please make sure
-# metadata has no dependencies, otherwise they will need to be added to
-# the setup_requires keyword.
-metadata = imp.load_source(
-    'metadata', os.path.join(CODE_DIRECTORY, 'metadata.py'))
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.5.0',  # Required
 
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description='Portfolio tools for GnuCash',  # Required
 
-# # Miscellaneous helper functions
+    # This is an optional longer description of your project that represents
+    # the body of text which users will see when they visit PyPI.
+    #
+    # Often, this is the same as your README, so you can just read it in from
+    # that file directly (as we have already done above)
+    #
+    # This field corresponds to the "Description" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#description-optional
+    long_description=long_description,  # Optional
 
-def get_project_files():
-    """Retrieve a list of project files, ignoring hidden files.
+    # This should be a valid link to your project's main homepage.
+    #
+    # This field corresponds to the "Home-Page" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    url='https://github.com/MisterY/gnucash-portfolio',  # Optional
 
-    :return: sorted list of project files
-    :rtype: :class:`list`
-    """
-    if is_git_project():
-        return get_git_project_files()
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author='Alen Siljak',  # Optional
 
-    project_files = []
-    for top, subdirs, files in os.walk('.'):
-        for subdir in subdirs:
-            if subdir.startswith('.'):
-                subdirs.remove(subdir)
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email='alen.siljak@gmx.com',  # Optional
 
-        for f in files:
-            if f.startswith('.'):
-                continue
-            project_files.append(os.path.join(top, f))
+    license='GPL version 3',
 
-    return project_files
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
 
-
-def is_git_project():
-    return os.path.isdir('.git')
-
-
-def get_git_project_files():
-    """Retrieve a list of all non-ignored files, including untracked files,
-    excluding deleted files.
-
-    :return: sorted list of git project files
-    :rtype: :class:`list`
-    """
-    cached_and_untracked_files = git_ls_files(
-        '--cached',  # All files cached in the index
-        '--others',  # Untracked files
-        # Exclude untracked files that would be excluded by .gitignore, etc.
-        '--exclude-standard')
-    uncommitted_deleted_files = git_ls_files('--deleted')
-
-    # Since sorting of files in a set is arbitrary, return a sorted list to
-    # provide a well-defined order to tools like flake8, etc.
-    return sorted(cached_and_untracked_files - uncommitted_deleted_files)
-
-
-def git_ls_files(*cmd_args):
-    """Run ``git ls-files`` in the top-level project directory. Arguments go
-    directly to execution call.
-
-    :return: set of file names
-    :rtype: :class:`set`
-    """
-    cmd = ['git', 'ls-files']
-    cmd += cmd_args
-    return set(subprocess.check_output(cmd).splitlines())
-
-
-def print_success_message(message):
-    """Print a message indicating success in green color to STDOUT.
-
-    :param message: the message to print
-    :type message: :class:`str`
-    """
-    try:
-        import colorama
-
-        print(colorama.Fore.GREEN + message + colorama.Fore.RESET)
-    except ImportError:
-        print(message)
-
-
-def print_failure_message(message):
-    """Print a message indicating failure in red color to STDERR.
-
-    :param message: the message to print
-    :type message: :class:`str`
-    """
-    try:
-        import colorama
-
-        print(colorama.Fore.RED + message + colorama.Fore.RESET,
-              file=sys.stderr)
-    except ImportError:
-        print(message, file=sys.stderr)
-
-
-def read(filename):
-    """Return the contents of a file.
-
-    :param filename: file path
-    :type filename: :class:`str`
-    :return: the file's content
-    :rtype: :class:`str`
-    """
-    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-        return f.read()
-
-
-def _lint():
-    """Run lint and return an exit code."""
-    # Flake8 doesn't have an easy way to run checks using a Python function, so
-    # just fork off another process to do it.
-
-    # Python 3 compat:
-    # - The result of subprocess call outputs are byte strings, meaning we need
-    # to pass a byte string to endswith.
-    project_python_files = [filename for filename in get_project_files()
-                            if filename.endswith(b'.py')]
-    retcode = subprocess.call(
-        ['flake8', '--ignore=E126,E121', '--max-line-length=99', '--max-complexity=10']
-        + project_python_files)
-    if retcode == 0:
-        print_success_message('No style errors')
-    return retcode
-
-
-def _test():
-    """Run the unit tests.
-
-    :return: exit code
-    """
-    # Make sure to import pytest in this function. For the reason, see here:
-    # <http://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands>  # NOPEP8
-    import pytest
-    # This runs the unit tests.
-    # It also runs doctest, but only on the modules in TESTS_DIRECTORY.
-    return pytest.main(PYTEST_FLAGS + [TESTS_DIRECTORY])
-
-
-def _test_all():
-    """Run lint and tests.
-
-    :return: exit code
-    """
-    return _lint() + _test()
-
-
-# The following code is to allow tests to be run with `python setup.py test'.
-# The main reason to make this possible is to allow tests to be run as part of
-# Setuptools' automatic run of 2to3 on the source code. The recommended way to
-# run tests is still `paver test_all'.
-# See <http://pythonhosted.org/setuptools/python3.html>
-# Code based on <http://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands>  # NOPEP8
-class TestAllCommand(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        # These are fake, and just set to appease distutils and setuptools.
-        self.test_suite = True
-        self.test_args = []
-
-    def run_tests(self):
-        raise SystemExit(_test())
-
-
-# define install_requires for specific Python versions
-python_version_specific_requires = []
-
-# as of Python >= 2.7 and >= 3.2, the argparse module is maintained within
-# the Python standard library, otherwise we install it as a separate package
-if sys.version_info < (2, 7) or (3, 0) <= sys.version_info < (3, 3):
-    python_version_specific_requires.append('argparse')
-
-BAT_TEMPLATE = \
-    r"""@echo off
-    REM wrapper to use shebang first line of {FNAME}
-    set mypath=%~dp0
-    set pyscript="%mypath%{FNAME}"
-    set /p line1=<%pyscript%
-    if "%line1:~0,2%" == "#!" (goto :goodstart)
-    echo First line of %pyscript% does not start with "#!"
-    exit /b 1
-    :goodstart
-    set py_exe=%line1:~2%
-    call "%py_exe%" %pyscript% %*
-    """
-
-
-class my_install_scripts(install_scripts):
-    def run(self):
-        install_scripts.run(self)
-        if not os.name == "nt":
-            return
-        for filepath in self.get_outputs():
-            # If we can find an executable name in the #! top line of the script
-            # file, make .bat wrapper for script.
-            with open(filepath, 'rt') as fobj:
-                first_line = fobj.readline()
-            # if not (first_line.startswith('#!') and
-            #         'python' in first_line.lower()):
-            #     log.info("No #!python executable found, skipping .bat "
-            #                 "wrapper")
-            #     continue
-            pth, fname = psplit(filepath)
-            froot, ext = splitext(fname)
-            bat_file = pjoin(pth, froot + '.bat')
-            bat_contents = BAT_TEMPLATE.replace('{FNAME}', fname)
-            log.info("Making %s wrapper for %s" % (bat_file, filepath))
-            if self.dry_run:
-                continue
-            with open(bat_file, 'wt') as fobj:
-                fobj.write(bat_contents)
-
-
-# See here for more options:
-# <http://pythonhosted.org/setuptools/setuptools.html>
-
-setup_dict = dict(
-    name=metadata.package,
-    version=metadata.version,
-    author=metadata.authors[0],
-    author_email=metadata.emails[0],
-    maintainer=metadata.authors[0],
-    maintainer_email=metadata.emails[0],
-    url=metadata.url,
-    description=metadata.description,
-#    long_description=read('README.rst'),
-    keywords=['GnuCash', 'portfolio', 'python', 'report'],
-    license='GPL3',
-    platforms='any',
-    # Find a list of classifiers here:
-    # <http://pypi.python.org/pypi?%3Aaction=list_classifiers>
-    classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Environment :: Console',
+        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Intended Audience :: Financial and Insurance Industry',
-        'License :: GPL Public License v3',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Office/Business :: Financial :: Accounting',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.6',
     ],
-    packages=find_packages(exclude=(TESTS_DIRECTORY,
-                                    DATA_DIRECTORY,
-                                    )),
-    include_package_data=True,
-    install_requires=[
-                         'piecash',
-                         'jinja2'
-                     ] + python_version_specific_requires,
-    # Allow tests to be run with `python setup.py test'.
-    tests_require=[
-        'pytest',
-        'mock',
-        'py',
-    ],
-    # scripts=['scripts/gc_test.py',
-    #          'scripts/gc_report.py',
-    #          'scripts/gc_report_create.py',
-    #          ],
-    cmdclass={'test': TestAllCommand,
-              'install_scripts': my_install_scripts},
-    zip_safe=False,  # don't use eggs
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='personal finance gnucash portfolio tools',  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['sqlalchemy', 'piecash'],  # Optional
+
+    python_requires='>=3.0',
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # If using Python 2.6 or earlier, then these have to be included in
+    # MANIFEST.in as well.
+
+    # package_data={  # Optional
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={  # Optional
+    #     'console_scripts': [
+    #         # 'aa=asset_allocation.asset_allocation:main',
+    #         'aa=asset_allocation.cli:cli',
+    #     ],
+    # },
+
+    # Include non-code files, listed in MANIFEST.in.
+    # http://python-packaging.readthedocs.io/en/latest/non-code-files.html
+    # include_package_data=True
 )
-
-
-def main():
-    setup(**setup_dict)
-
-
-if __name__ == '__main__':
-    main()
