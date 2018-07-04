7a0b0d0cd719dc6b09fc36ff7cd9f0b9a51fd788
Alen Siljak <alen.siljak@gmx.com>
2018-07-04 17:41:58 +0200

    adding files to the root is not a good option

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ setup(
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
 
     # data_files=[('my_data', ['data/data_file'])],  # Optional
-    data_files=[('config', ['config/settings.json.template'])],
+    #data_files=[('config', ['config/settings.json.template'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
