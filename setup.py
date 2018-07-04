aa687360f1de26b5108a1cf1e12a10b4d0489dc6
Alen Siljak <alen.siljak@gmx.com>
2018-07-04 17:52:33 +0200

    the config template is now packaged v1.0.11

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.8',  # Required
+    version='1.0.11',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -150,7 +150,7 @@ setup(
     # },
     # package_data={
     #     '': ['README.md'],
-    #     'config': ['config/settings.json.template']
+    #     'gnucash_portfolio': ['gnucash_portfolio/config/settings.json.template']
     # },
 
     # Although 'package_data' is the preferred approach, in some case you may
