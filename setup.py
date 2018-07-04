907d51fb0a11bd5a3aaa4bf0aab09f6d26048256
Alen Siljak <alen.siljak@gmx.com>
2018-07-04 15:27:52 +0200

    adding settings template to the package v1.0.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',  # Required
+    version='1.0.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -173,5 +173,5 @@ setup(
 
     # Include non-code files, listed in MANIFEST.in.
     # http://python-packaging.readthedocs.io/en/latest/non-code-files.html
-    # include_package_data=True
+    include_package_data=True
 )
