703377bb776f3922ef97e1b361d3f57073b3988c
Alen Siljak <alen.siljak@gmx.com>
2018-07-04 17:28:59 +0200

    using manifest.in v1.0.5

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.4',  # Required
+    version='1.0.5',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -160,7 +160,7 @@ setup(
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
 
     # data_files=[('my_data', ['data/data_file'])],  # Optional
-    data_files=[('config', ['config/settings.json.template'])],
+    #data_files=[('config', ['config/settings.json.template'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
