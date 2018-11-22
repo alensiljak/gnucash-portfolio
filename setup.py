1ba2f0b794f1e7165603c654beb066485070d70e
Alen Siljak <alen.siljak@yahoo.com>
2018-11-22 22:04:15 +0100

    v1.5.2, correcting the dependency package name

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.5.1',  # Required
+    version='1.5.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -122,7 +122,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'price-database',
+        'PriceDb',
         'pydatum',
         'sqlalchemy',
         'piecash',
